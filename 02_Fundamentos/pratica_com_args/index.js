26c537ae93703b24f36977bd90428d54edda0df1
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-19 22:04:20 -0300

    Executando a função recebida, com os parâmetros passados pelo usuário

--- 02_Fundamentos/pratica_com_args/index.js
+++ 02_Fundamentos/pratica_com_args/index.js
@@ -1,4 +1,12 @@
 const minimist = require('minimist');
 
+// módulo externo
 const args = minimist(process.argv.slice(2));
 
+// módulo interno
+const soma = require('./soma').soma;
+
+const a = parseInt(args["a"]);
+const b = parseInt(args["b"]);
+
+soma(a, b);
