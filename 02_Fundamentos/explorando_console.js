2717016a87387847f2f4660d42d9685117b7d40a
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-22 16:28:35 -0300

    Explorando o console

---
+++ 02_Fundamentos/explorando_console.js
@@ -0,0 +1,23 @@
+const x = 10
+const y = 'Algum texto'
+const z = [1,2]
+
+console.log(x, y, z)
+
+console.count(`O valor de x é ${x}, contagem`)
+console.count(`O valor de x é ${x}, contagem`)
+console.count(`O valor de x é ${x}, contagem`)
+console.count(`O valor de x é ${x}, contagem`)
+console.count(`O valor de x é ${x}, contagem`)
+console.count(`O valor de x é ${x}, contagem`)
+console.count(`O valor de x é ${x}, contagem`)
+console.count(`O valor de x é ${x}, contagem`)
+console.count(`O valor de x é ${x}, contagem`)
+console.count(`O valor de x é ${x}, contagem`)
+
+
+console.log("O nome é %s e ele é Programador.", y)
+
+setTimeout(() => {
+    console.clear()
+}, 1000)
