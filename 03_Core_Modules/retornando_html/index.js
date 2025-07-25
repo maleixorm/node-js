e4f8b7374007f652c628ff7ebc91b841fe5f088f
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-25 11:18:30 -0300

    Atualizando a aplicação com novos dados

--- 03_Core_Modules/retornando_html/index.js
+++ 03_Core_Modules/retornando_html/index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end("<h1>Olá! Este é meu primeiro servidor com HTML!</h1>");
+    res.end("<h1>Olá! Este é meu primeiro servidor com HTML!</h1><p>Testando Atualização</p>");
 });
 
 server.listen(port, () => {
