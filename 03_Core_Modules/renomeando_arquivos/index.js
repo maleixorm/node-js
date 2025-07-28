f4239d55dc3ebc1d6a794d9be07551a3aa624a27
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-28 16:05:45 -0300

    Renomeando arquivos com o core module FS

---
+++ 03_Core_Modules/renomeando_arquivos/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+fs.rename("arquivo.txt", "novoarquivo.txt", (err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log("Arquivo renomeado!");
+});
